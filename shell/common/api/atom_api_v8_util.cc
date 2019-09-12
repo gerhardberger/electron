072ed03d5dc21116f859f139553813be29bee88d
Cheng Zhao <zcbenz@gmail.com>
2019-09-12 13:04:18 -0400

    remove usage of object_template_builder_deprecated.h in a few files

--- shell/common/api/atom_api_v8_util.cc
+++ shell/common/api/atom_api_v8_util.cc
@@ -6,15 +6,19 @@
 #include <utility>
 
 #include "base/hash/hash.h"
-#include "native_mate/dictionary.h"
 #include "shell/common/api/atom_api_key_weak_map.h"
 #include "shell/common/api/remote_callback_freer.h"
 #include "shell/common/api/remote_object_freer.h"
-#include "shell/common/native_mate_converters/content_converter.h"
-#include "shell/common/native_mate_converters/gurl_converter.h"
+#include "shell/common/gin_converters/gurl_converter.h"
+#include "shell/common/gin_converters/native_mate_handle_converter.h"
+#include "shell/common/gin_helper/dictionary.h"
 #include "shell/common/node_includes.h"
 #include "url/origin.h"
 #include "v8/include/v8-profiler.h"
+
+// TODO(zcbenz): Remove the includes after removing native_mate.
+#include "native_mate/dictionary.h"
+#include "shell/common/native_mate_converters/content_converter.h"
 
 namespace std {
 
@@ -28,7 +32,7 @@ struct hash<std::pair<Type1, Type2>> {
 
 }  // namespace std
 
-namespace mate {
+namespace gin {
 
 template <typename Type1, typename Type2>
 struct Converter<std::pair<Type1, Type2>> {
@@ -50,7 +54,7 @@ struct Converter<std::pair<Type1, Type2>> {
   }
 };
 
-}  // namespace mate
+}  // namespace gin
 
 namespace {
 
@@ -111,14 +115,17 @@ void Initialize(v8::Local<v8::Object> exports,
                 v8::Local<v8::Value> unused,
                 v8::Local<v8::Context> context,
                 void* priv) {
-  mate::Dictionary dict(context->GetIsolate(), exports);
+  gin_helper::Dictionary dict(context->GetIsolate(), exports);
   dict.SetMethod("getHiddenValue", &GetHiddenValue);
   dict.SetMethod("setHiddenValue", &SetHiddenValue);
   dict.SetMethod("deleteHiddenValue", &DeleteHiddenValue);
   dict.SetMethod("getObjectHash", &GetObjectHash);
   dict.SetMethod("takeHeapSnapshot", &TakeHeapSnapshot);
-  dict.SetMethod("setRemoteCallbackFreer",
-                 &electron::RemoteCallbackFreer::BindTo);
+  // TODO(zcbenz): Use gin_helper::Dictionary when content_converter.h is moved
+  // to gin.
+  mate::Dictionary mdict(context->GetIsolate(), exports);
+  mdict.SetMethod("setRemoteCallbackFreer",
+                  &electron::RemoteCallbackFreer::BindTo);
   dict.SetMethod("setRemoteObjectFreer", &electron::RemoteObjectFreer::BindTo);
   dict.SetMethod("addRemoteObjectRef", &electron::RemoteObjectFreer::AddRef);
   dict.SetMethod("createIDWeakMap",
