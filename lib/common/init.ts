91835b3e6113f6cebe26829a13c4d4973bf2fc9b
gellert <gellert.hegyi@around.co>
2021-08-13 12:02:48 +0200

    fix: electron_worklet_fix

--- lib/common/init.ts
+++ lib/common/init.ts
@@ -2,22 +2,7 @@ import * as util from 'util';
 
 const timers = require('timers');
 
-type AnyFn = (...args: any[]) => any
-
-// setImmediate and process.nextTick makes use of uv_check and uv_prepare to
-// run the callbacks, however since we only run uv loop on requests, the
-// callbacks wouldn't be called until something else activated the uv loop,
-// which would delay the callbacks for arbitrary long time. So we should
-// initiatively activate the uv loop once setImmediate and process.nextTick is
-// called.
-const wrapWithActivateUvLoop = function <T extends AnyFn> (func: T): T {
-  return wrap(func, function (func) {
-    return function (this: any, ...args: any[]) {
-      process.activateUvLoop();
-      return func.apply(this, args);
-    };
-  }) as T;
-};
+type AnyFn = (...args: any[]) => any;
 
 /**
  * Casts to any below for func are due to Typescript not supporting symbols
@@ -33,34 +18,51 @@ function wrap <T extends AnyFn> (func: T, wrapper: (fn: AnyFn) => T) {
   return wrapped;
 }
 
-process.nextTick = wrapWithActivateUvLoop(process.nextTick);
+if (process) {
+  // setImmediate and process.nextTick makes use of uv_check and uv_prepare to
+  // run the callbacks, however since we only run uv loop on requests, the
+  // callbacks wouldn't be called until something else activated the uv loop,
+  // which would delay the callbacks for arbitrary long time. So we should
+  // initiatively activate the uv loop once setImmediate and process.nextTick is
+  // called.
+  const wrapWithActivateUvLoop = function <T extends AnyFn> (func: T): T {
+    return wrap(func, function (func) {
+      return function (this: any, ...args: any[]) {
+        process.activateUvLoop();
+        return func.apply(this, args);
+      };
+    }) as T;
+  };
 
-global.setImmediate = timers.setImmediate = wrapWithActivateUvLoop(timers.setImmediate);
-global.clearImmediate = timers.clearImmediate;
+  process.nextTick = wrapWithActivateUvLoop(process.nextTick);
 
-// setTimeout needs to update the polling timeout of the event loop, when
-// called under Chromium's event loop the node's event loop won't get a chance
-// to update the timeout, so we have to force the node's event loop to
-// recalculate the timeout in browser process.
-timers.setTimeout = wrapWithActivateUvLoop(timers.setTimeout);
-timers.setInterval = wrapWithActivateUvLoop(timers.setInterval);
+  global.setImmediate = timers.setImmediate = wrapWithActivateUvLoop(timers.setImmediate);
+  global.clearImmediate = timers.clearImmediate;
 
-// Only override the global setTimeout/setInterval impls in the browser process
-if (process.type === 'browser') {
-  global.setTimeout = timers.setTimeout;
-  global.setInterval = timers.setInterval;
-}
+  // setTimeout needs to update the polling timeout of the event loop, when
+  // called under Chromium's event loop the node's event loop won't get a chance
+  // to update the timeout, so we have to force the node's event loop to
+  // recalculate the timeout in browser process.
+  timers.setTimeout = wrapWithActivateUvLoop(timers.setTimeout);
+  timers.setInterval = wrapWithActivateUvLoop(timers.setInterval);
 
-if (process.platform === 'win32') {
-  // Always returns EOF for stdin stream.
-  const { Readable } = require('stream');
-  const stdin = new Readable();
-  stdin.push(null);
-  Object.defineProperty(process, 'stdin', {
-    configurable: false,
-    enumerable: true,
-    get () {
-      return stdin;
-    }
-  });
+  // Only override the global setTimeout/setInterval impls in the browser process
+  if (process.type === 'browser') {
+    global.setTimeout = timers.setTimeout;
+    global.setInterval = timers.setInterval;
+  }
+
+  if (process.platform === 'win32') {
+    // Always returns EOF for stdin stream.
+    const { Readable } = require('stream');
+    const stdin = new Readable();
+    stdin.push(null);
+    Object.defineProperty(process, 'stdin', {
+      configurable: false,
+      enumerable: true,
+      get () {
+        return stdin;
+      }
+    });
+  }
 }
--- lib/worker/init.ts
+++ lib/worker/init.ts
@@ -1,44 +1,48 @@
 import * as path from 'path';
 
-const Module = require('module');
+const inWorklet = !process && !global;
 
-// We modified the original process.argv to let node.js load the
-// init.js, we need to restore it here.
-process.argv.splice(1, 1);
+if (!inWorklet) {
+  const Module = require('module');
 
-// Clear search paths.
-require('../common/reset-search-paths');
+  // We modified the original process.argv to let node.js load the
+  // init.js, we need to restore it here.
+  process.argv.splice(1, 1);
 
-// Import common settings.
-require('@electron/internal/common/init');
+  // Clear search paths.
+  require('../common/reset-search-paths');
 
-// Process command line arguments.
-const { hasSwitch, getSwitchValue } = process._linkedBinding('electron_common_command_line');
+  // Import common settings.
+  require('@electron/internal/common/init');
 
-// Export node bindings to global.
-const { makeRequireFunction } = __non_webpack_require__('internal/modules/cjs/helpers') // eslint-disable-line
-global.module = new Module('electron/js2c/worker_init');
-global.require = makeRequireFunction(global.module);
+  // Process command line arguments.
+  const { hasSwitch, getSwitchValue } = process._linkedBinding('electron_common_command_line');
 
-// Set the __filename to the path of html file if it is file: protocol.
-if (self.location.protocol === 'file:') {
-  const pathname = process.platform === 'win32' && self.location.pathname[0] === '/' ? self.location.pathname.substr(1) : self.location.pathname;
-  global.__filename = path.normalize(decodeURIComponent(pathname));
-  global.__dirname = path.dirname(global.__filename);
+  // Export node bindings to global.
+  const { makeRequireFunction } = __non_webpack_require__('internal/modules/cjs/helpers') // eslint-disable-line
+  global.module = new Module('electron/js2c/worker_init');
+  global.require = makeRequireFunction(global.module);
 
-  // Set module's filename so relative require can work as expected.
-  global.module.filename = global.__filename;
+  // Set the __filename to the path of html file if it is file: protocol.
+  if (self.location.protocol === 'file:') {
+    const pathname = process.platform === 'win32' && self.location.pathname[0] === '/' ? self.location.pathname.substr(1) : self.location.pathname;
+    global.__filename = path.normalize(decodeURIComponent(pathname));
+    global.__dirname = path.dirname(global.__filename);
 
-  // Also search for module under the html file.
-  global.module.paths = Module._nodeModulePaths(global.__dirname);
-} else {
-  // For backwards compatibility we fake these two paths here
-  global.__filename = path.join(process.resourcesPath, 'electron.asar', 'worker', 'init.js');
-  global.__dirname = path.join(process.resourcesPath, 'electron.asar', 'worker');
+    // Set module's filename so relative require can work as expected.
+    global.module.filename = global.__filename;
 
-  const appPath = hasSwitch('app-path') ? getSwitchValue('app-path') : null;
-  if (appPath) {
-    // Search for module under the app directory.
-    global.module.paths = Module._nodeModulePaths(appPath);
+    // Also search for module under the html file.
+    global.module.paths = Module._nodeModulePaths(global.__dirname);
+  } else {
+    // For backwards compatibility we fake these two paths here
+    global.__filename = path.join(process.resourcesPath, 'electron.asar', 'worker', 'init.js');
+    global.__dirname = path.join(process.resourcesPath, 'electron.asar', 'worker');
+
+    const appPath = hasSwitch('app-path') ? getSwitchValue('app-path') : null;
+    if (appPath) {
+      // Search for module under the app directory.
+      global.module.paths = Module._nodeModulePaths(appPath);
+    }
   }
 }
